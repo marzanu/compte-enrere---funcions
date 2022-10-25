678900fa7d0245a436ed51910f21abb5e25ebab3
marzanu <116082767+marzanu@users.noreply.github.com>
2022-10-25 14:33:22 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    estat_de_LEDS(1000)
+})
+function estat_de_LEDS (temps: number) {
+    for (let index_y = 0; index_y <= 4; index_y++) {
+        for (let index_x = 0; index_x <= 4; index_x++) {
+            led.toggle(index_x, index_y)
+        }
+        basic.pause(temps)
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    for (let index_y = 0; index_y <= 4; index_y++) {
+        for (let index_x = 0; index_x <= 4; index_x++) {
+            led.unplot(index_x, Math.map(index_y, 0, 4, 4, 0))
+        }
+        basic.pause(1000)
+    }
+    for (let index = 0; index < 5; index++) {
+        for (let index_y = 0; index_y <= 4; index_y++) {
+            for (let index_x = 0; index_x <= 4; index_x++) {
+                led.toggle(index_x, index_y)
+            }
+        }
+        basic.pause(200)
+    }
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+estat_de_LEDS(1)
